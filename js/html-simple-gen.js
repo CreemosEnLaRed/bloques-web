659dfbec637e0e27d0b5471c1ea24dd7f532bdaa
Mariano Guerra <mariano@marianoguerra.org>
2020-01-20 19:29:58 +0100

    more blocks better toolbox

--- js/html-simple-gen.js
+++ js/html-simple-gen.js
@@ -1,36 +1,66 @@
 Blockly.JavaScript['hs_par'] = function(block) {
-  var statements_items = Blockly.JavaScript.statementToCode(block, 'items');
+  var statements_items = Blockly.JavaScript.statementToCode(block, 'ITEMS');
   // TODO: Assemble JavaScript into code variable.
   var code = '...;\n';
   return code;
 };
 
 Blockly.JavaScript['hs_title'] = function(block) {
-  var dropdown_level = block.getFieldValue('level');
-  var value_value = Blockly.JavaScript.valueToCode(block, 'value', Blockly.JavaScript.ORDER_ATOMIC);
+  var dropdown_level = block.getFieldValue('LEVEL');
+  var statements_items = Blockly.JavaScript.statementToCode(block, 'ITEMS');
   // TODO: Assemble JavaScript into code variable.
   var code = '...;\n';
   return code;
 };
 
 Blockly.JavaScript['hs_text'] = function(block) {
-  var dropdown_type = block.getFieldValue('type');
-  var value_value = Blockly.JavaScript.valueToCode(block, 'value', Blockly.JavaScript.ORDER_ATOMIC);
+  var text_text = block.getFieldValue('TEXT');
   // TODO: Assemble JavaScript into code variable.
   var code = '...;\n';
   return code;
 };
 
 Blockly.JavaScript['hs_list'] = function(block) {
-  var dropdown_type = block.getFieldValue('type');
-  var statements_item = Blockly.JavaScript.statementToCode(block, 'item');
+  var dropdown_type = block.getFieldValue('TYPE');
+  var statements_items = Blockly.JavaScript.statementToCode(block, 'ITEMS');
   // TODO: Assemble JavaScript into code variable.
   var code = '...;\n';
   return code;
 };
 
 Blockly.JavaScript['hs_li'] = function(block) {
-  var statements_items = Blockly.JavaScript.statementToCode(block, 'items');
+  var statements_items = Blockly.JavaScript.statementToCode(block, 'ITEMS');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['hs_div'] = function(block) {
+  var statements_items = Blockly.JavaScript.statementToCode(block, 'ITEMS');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['hs_wraptext'] = function(block) {
+  var dropdown_type = block.getFieldValue('TYPE');
+  var statements_items = Blockly.JavaScript.statementToCode(block, 'ITEMS');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['hs_img'] = function(block) {
+  var text_src = block.getFieldValue('SRC');
+  var text_alt = block.getFieldValue('ALT');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['hs_link'] = function(block) {
+  var text_href = block.getFieldValue('HREF');
+  var text_label = block.getFieldValue('LABEL');
   // TODO: Assemble JavaScript into code variable.
   var code = '...;\n';
   return code;
